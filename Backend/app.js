37e8fe6a52bc4b87425a102c951cdf9388a49330
joeljones29 <166531014+joeljones29@users.noreply.github.com>
2025-01-09 18:31:32 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -9,7 +9,7 @@ app.get('/api', (req, res) => {
   res.json({ message: 'Hello from the backend!' });
 });
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from the backend!' });
+  res.json({ message: 'This is the main window' });
 });
 // Start the backend server
 app.listen(PORT, () => {
